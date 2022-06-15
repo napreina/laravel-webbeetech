405a40c94197bc9bb5bd00edcb9e0f2f2090c503
tony-fullstack-515 <tony.fullstack.dev@gmail.com>
2022-06-15 03:15:34 -0400

    Updated getMenuItems function with recurring eager loading wit with() in MenuItem Model

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class MenuController extends BaseController
@@ -95,6 +96,16 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        try {
+            $menu_items = MenuItem::query()
+                ->where('parent_id', null)
+                ->with('children')
+                ->get();
+
+            return response()->json($menu_items, Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            throw new \Exception('implement in coding task 3');
+        }
+
     }
 }
